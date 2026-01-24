790b7363f7683258c5ec5f87abea50ca9412d82a
Shreyas Shetti <129672540+Sunitashetti@users.noreply.github.com>
2026-01-24 21:49:49 +0530

    Create day03_operators.py

---
+++ day03_operators.py
@@ -0,0 +1,9 @@
+a = 10
+b = 5
+
+print(a + b)        # Arithmetic
+print(a > b)        # Comparison
+print(a > 5 and b < 10)  # Logical
+a += 2              # Assignment
+print(a)
+print(a & b)        # Bitwise
