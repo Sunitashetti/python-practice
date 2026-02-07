19c8af52f4391dd702993a60c27a5343ca168cd0
Shreyas Shetti <129672540+Sunitashetti@users.noreply.github.com>
2026-02-07 19:33:00 +0530

    Create traffic_light.py

---
+++ traffic_light.py
@@ -0,0 +1,11 @@
+
+color = input("Enter traffic light color: ").lower()
+
+if color == "red":
+    print("Stop")
+elif color == "yellow":
+    print("Get Ready")
+elif color == "green":
+    print("Go")
+else:
+    print("Invalid color")
