14b01e36844ee6bdcae01c1ef62b3103383cb6a1
Shreyas Shetti <129672540+Sunitashetti@users.noreply.github.com>
2026-02-07 19:33:59 +0530

    Create login_simulator.py

---
+++ login_simulator.py
@@ -0,0 +1,10 @@
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin" and password == "1234":
+    print("Login successful")
+elif username == "admin":
+    print("Wrong password")
+else:
+    print("User not found")
