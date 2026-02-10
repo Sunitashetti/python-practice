02b960f5a3aea1191040c72d6e775ae587d55a7e
Shreyas Shetti <129672540+Sunitashetti@users.noreply.github.com>
2026-02-10 22:35:37 +0530

    Create day17_for_loop_practice.py

---
+++ day17_for_loop_practice.py
@@ -0,0 +1,20 @@
+numbers = [10, 20, 30, 40, 50]
+total = 0
+
+for num in numbers:
+    total += num
+
+print("Total sum:", total)
+
+foods = ["Dosa", "Idli", "Vada", "Bisibelebath"]
+
+for food in foods:
+    print(f"I like {food}")
+
+numbers = [1, 2, 3, 4, 5]
+doubled = []
+
+for num in numbers:
+    doubled.append(num * 2)
+
+print("Doubled List:", doubled)
